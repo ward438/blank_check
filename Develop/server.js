4c8d059b9a4dada17a5df13b6107f89890db22e2
Robert W <75048548+ward438@users.noreply.github.com>
2021-06-04 13:58:37 -0500

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/transactions", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
